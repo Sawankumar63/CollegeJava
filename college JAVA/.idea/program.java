7957d34e1382e7f0a037c1ef263205272097ec7f
Sawankumar63 <sawankumar6369@gmail.com>
2026-02-19 09:35:59 +0530

    first commit

---
+++ college JAVA/.idea/program.java
@@ -0,0 +1,12 @@
+public class program {
+    public static void main(String[] args) {
+        int x=10;
+        int y=3;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x%y);
+
+    }
+}
