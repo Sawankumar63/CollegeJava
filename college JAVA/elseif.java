75e4e769fcff5eb41b93dd89b07ee920cea4e204
Sawankumar63 <sawankumar6369@gmail.com>
2026-03-26 10:40:52 +0530

    notes

---
+++ college JAVA/elseif.java
@@ -0,0 +1,18 @@
+public class elseif { 
+
+public static void main(String[] args) {
+    int number = 10;
+
+    if (number <= 18) {
+        System.out.println("Choclete.");
+    } else if (number >=18) {
+        System.out.println("pizza.");
+        else if (number >=25) {
+        System.out.println("paper Boat.");
+        else if (number >=35 && number >=45) {
+        System.out.println("Samosa.");
+    } else {
+        System.out.println("grater then 45.");
+    }
+}
+}
